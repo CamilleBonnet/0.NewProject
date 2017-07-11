ec4ee95f8b0ac75eeba02a21de7a0f5610d8a3d5
MLK <bonnet.cam@gmail.com>
2017-07-11 14:10:49 +0800

    several modifications on the site - favicon Done !

--- javascript/javascript_file.js
+++ javascript/javascript_file.js
@@ -1,11 +1,15 @@
 $(document).ready(function(){
+	/* Select the click button*/
 	var $bjc=$(".btn-java-click");
 	$bjc.click(function(){
+		/* Apply or remove a specific class on the button to apply an effect*/
 		$bjc.toggleClass("clickEvent");
 	});
 
+	/* Select the hover button*/
 	var $bjh=$(".btn-java-hover");
 	$bjh.hover(function(){
+		/* Apply or remove a specific class on the button to apply an effect*/
 		$bjh.toggleClass("hoverEvent");
 	});
 
